f6738885b32e7000cfb4a57c30ddd40856b360cc
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-02-27 13:17:04 +0800

    feat: add more Node APIs

--- src/tree-sitter.c
+++ src/tree-sitter.c
@@ -1,6 +1,7 @@
 #include <dlfcn.h>
 #include <moonbit.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 #include <tree_sitter/api.h>
 
@@ -66,14 +67,14 @@ moonbit_ts_node_named_child(TSNode *self, uint32_t child_index) {
   return node;
 }
 
-int
+uint32_t
 moonbit_ts_node_child_count(TSNode *self) {
   int count = ts_node_child_count(*self);
   moonbit_decref(self);
   return count;
 }
 
-int
+uint32_t
 moonbit_ts_node_named_child_count(TSNode *self) {
   int count = ts_node_named_child_count(*self);
   moonbit_decref(self);
@@ -112,4 +113,84 @@ moonbit_ts_node_end_byte(TSNode *self) {
   uint32_t byte = ts_node_end_byte(*self);
   moonbit_decref(self);
   return byte;
+}
+
+TSPoint *
+moonbit_ts_node_start_point(TSNode *self) {
+  TSPoint *point = (TSPoint *)moonbit_malloc(sizeof(struct TSPoint));
+  *point = ts_node_start_point(*self);
+  moonbit_decref(self);
+  return point;
+}
+
+TSPoint *
+moonbit_ts_node_end_point(TSNode *self) {
+  TSPoint *point = (TSPoint *)moonbit_malloc(sizeof(struct TSPoint));
+  *point = ts_node_end_point(*self);
+  moonbit_decref(self);
+  return point;
+}
+
+TSNode *
+moonbit_ts_node_next_sibling(TSNode *self) {
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_next_sibling(*self);
+  moonbit_decref(self);
+  return node;
+}
+
+TSNode *
+moonbit_ts_node_prev_sibling(TSNode *self) {
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_prev_sibling(*self);
+  moonbit_decref(self);
+  return node;
+}
+
+TSNode *
+moonbit_ts_node_next_named_sibling(TSNode *self) {
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_next_named_sibling(*self);
+  moonbit_decref(self);
+  return node;
+}
+
+TSNode *
+moonbit_ts_node_prev_named_sibling(TSNode *self) {
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_prev_named_sibling(*self);
+  moonbit_decref(self);
+  return node;
+}
+
+bool
+moonbit_ts_node_is_null(TSNode *self) {
+  bool is_null = ts_node_is_null(*self);
+  moonbit_decref(self);
+  return is_null;
+}
+
+bool
+moonbit_ts_node_is_named(TSNode *self) {
+  bool is_named = ts_node_is_named(*self);
+  moonbit_decref(self);
+  return is_named;
+}
+
+TSNode *
+moonbit_ts_node_parent(TSNode *self) {
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_parent(*self);
+  moonbit_decref(self);
+  return node;
+}
+
+TSNode *
+moonbit_ts_node_child_by_field_name(TSNode *self, moonbit_bytes_t name) {
+  uint32_t length = Moonbit_array_length(name);
+  TSNode *node = (TSNode *)moonbit_malloc(sizeof(struct TSNode));
+  *node = ts_node_child_by_field_name(*self, (const char *)name, length);
+  moonbit_decref(self);
+  moonbit_decref(name);
+  return node;
 }
